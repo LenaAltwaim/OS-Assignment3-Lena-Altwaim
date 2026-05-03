98e4b62f56c85d5f5f192a74563fce83e7ebf468
LenaAltwaim <445052077@std.psau.edu.sa>
2026-05-03 21:35:39 +0300

    Protect shared counter ( contextSwitchCount++) using ReentantLock named lock and release

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -32,10 +32,7 @@ class Colors {
 class SharedResources {
     // TODO: Students will add synchronization mechanisms here
     // Added lock for protecting shared variables
-    public static final ReentrantLock lock = new ReentrantLock();
-
     // Added semaphore for CPU control (1 permit = 1 process at a time)
-    public static final Semaphore cpuSemaphore = new Semaphore(1);
     // HINT: Use ReentrantLock for mutual exclusion
     // HINT: Use Semaphore for limiting concurrent access
 
@@ -46,15 +43,22 @@ class SharedResources {
 
     // TODO #1: Add a ReentrantLock(s) here to protect critical sections
     // Example: public static final ReentrantLock lock = new ReentrantLock();
+    public static final ReentrantLock lock = new ReentrantLock();
 
     // TODO #2: Add a Semaphore to limit concurrent process execution
     // Example: public static final Semaphore cpuSemaphore = new Semaphore(1);
+    public static final Semaphore cpuSemaphore = new Semaphore(1);
 
     // Method to increment context switch counter
     public static void incrementContextSwitch() {
         // TODO: Protect this critical section with a lock
         // RACE CONDITION: Multiple threads might read and write simultaneously!
-        contextSwitchCount++;
+        lock.lock();
+        try {
+            contextSwitchCount++;
+        } finally {
+            lock.unlock();
+        }
     }
 
     // Method to increment completed process counter
